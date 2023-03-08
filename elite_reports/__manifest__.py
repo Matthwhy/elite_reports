549425d70fb4c300b0a766cdcf84f5aefc34c1a2
Matheus Marques <marquesmathw@gmail.com>
2023-03-08 17:35:50 -0300

    etiqueta

---
+++ elite_reports/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Etiqueta Personalizada Elite",
+    'summary': """  Addons que aplica as etiquetas na fatura da elite """,
+    'category': 'account',
+    'version': '14.01',
+    "author": "Trustcode",
+    "website": "https://trustcode.com.br",
+    'depends': [
+        'account',
+
+    ],
+    'data': [
+        'report/report_faturado_etiqueta.xml',
+    ],
+}
